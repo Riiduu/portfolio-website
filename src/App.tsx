adc2c87c5ea7ab6612270d660541d592110c5ee8
Soapman-PC <jelinsksk@gmail.com>
2024-09-19 13:03:28 +0300

    Finished hero section ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App__container">
         <Navbar />
-        <HeroComponent />
+        <HeroComponent readMoreButton="#"/>
     </div>
   )
 }
--- src/components/hero-components/HeroComponent.tsx
+++ src/components/hero-components/HeroComponent.tsx
@@ -1,16 +1,35 @@
 import './HeroComponent.css'
 
-const HeroComponent = () => {
+import ArrowDown from '../../assets/project-assets/Vector 187.png';
+import HeroImg from '../../assets/project-assets/hero-img.png';
+import ButtonDown from '../../assets/project-assets/button-down.png';
+
+const HeroComponent = (props: any) => {
     return (
         <div className="HeroComponent__container">
             {/* I use arch container*/}
-            <div>
+            <div className="HeroComponent__container_left_box">
+                <img src={ArrowDown} height="250px" alt=""/>
+                <div className="HeroComponent__left_box_info">
+                    <h1>I USE<br/><span className="HeroComponent__i_use_arch_btw">ARCH, btw</span></h1>
+                    {/* Hire me & CV buttons */}
+                    <div className="HeroComponent__info_buttons">
+                        {/* Hire me button */}
+                        <button className="HeroComponent__first_btn">Hire me</button>
+                        {/* Download CV button */}
+                        <button className="HeroComponent__second_btn">Download CV</button>
+                    </div>
+                </div>
 
             </div>
 
+            <a className="HeroComponent__read_more_button" href={props.readMoreButton}>
+                <img src={ButtonDown} alt=""/>
+            </a>
+
             {/* image container*/}
             <div>
-
+                <img src={HeroImg} alt=""/>
 
             </div>
         </div>
