aea50ca83c0730b824059896a365d7512c2c9bd1
Soapman-PC <jelinsksk@gmail.com>
2024-09-25 18:43:40 +0300

    Small adjustment

--- src/components/hero-components/HeroComponent.tsx
+++ src/components/hero-components/HeroComponent.tsx
@@ -13,19 +13,25 @@ const HeroComponent = (props: any) => {
     }
 
     return (
-        <div className="h-96 my-60 flex flex-row justify-center md:justify-between relative flex-wrap mx-4 md:mx-4 lg:mx-8 xl:mx-0">
+        <div
+            className="h-96 my-60 flex flex-row justify-center md:justify-between relative flex-wrap mx-4 md:mx-4 lg:mx-8 xl:mx-0">
             {/* I use arch container*/}
             <div className="flex flex-row mb-40 my-auto z-10 overflow-visible">
                 <img className="mt-8 h-40 hidden lg:block" src={ArrowDown} alt=""/>
                 <div className="">
-                    <h1 className="text-7xl md:text-6xl font-bold text-white">I DON'T USE<br/><span className="text-[var(--color-3)]">ARCH, btw</span></h1>
+                    <h1 className="text-7xl md:text-6xl text-center md:text-left font-bold text-white">I DON'T
+                        USE<br/><span className="text-[var(--color-3)]">ARCH, btw</span></h1>
                     {/* Hire me & CV buttons */}
                     <div className="mt-8 space-x-5 flex flex-row justify-center md:justify-start">
                         {/* Hire me button */}
-                        <button className="text-white bg-[var(--color-3)] py-2 px-10 rounded-3xl" onClick={routeToContact}>Hire me</button>
+                        <button className="text-white bg-[var(--color-3)] py-2 px-10 rounded-3xl"
+                                onClick={routeToContact}>Hire me
+                        </button>
                         {/* Download CV button */}
-                        <button onClick={routeToCV} className="text-white bg-[#393E46] py-2 px-10 rounded-3xl flex flex-row justify-center">View CV <span
-                            className="ml-2 material-symbols-outlined">open_in_new</span></button>
+                        <button onClick={routeToCV}
+                                className="text-white bg-[#393E46] py-2 px-10 rounded-3xl flex flex-row justify-center">View
+                            CV <span
+                                className="ml-2 material-symbols-outlined">open_in_new</span></button>
                     </div>
                 </div>
 
@@ -36,7 +42,8 @@ const HeroComponent = (props: any) => {
             </a>
 
             {/* image container*/}
-            <div className="z-0 absolute w-full md:w-auto md:-translate-x-0 md:right-0 top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2">
+            <div
+                className="z-0 absolute w-full md:w-auto md:-translate-x-0 md:right-0 top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2">
                 <img className="opacity-30 md:opacity-100 m-auto" src={HeroImg} alt=""/>
             </div>
         </div>
